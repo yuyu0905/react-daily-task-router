26c52d904356282d8ff7ae99a8d34ddb924c4a4f
TzuHui <angel1996421@gmail.com>
2022-07-28 10:22:17 +0800

    git init

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import {
+  BrowserRouter,
+  NavLink,
+} from 'react-router-dom';
+
+const Todo = () => {
+  return <p>這是 Todo 頁面 
+  </p>;
+};
+const Login = () => {
+  return <p>這是登入頁面</p>;
+};
+const Register = () => {
+  return <p>這是註冊頁面</p>;
+};
+
+function App() {
+  return (
+    <div className="container">
+      <BrowserRouter>
+        <div className="nav-link">
+          <NavLink to="/">
+            <p>回到首頁</p>
+          </NavLink>
+          <NavLink to="/register">
+            <p>註冊頁面</p>
+          </NavLink>
+          <NavLink to="/login">
+            <p>登入頁面</p>
+          </NavLink>
+          <NavLink to="/todo">
+            <p>Todo 頁面</p>
+          </NavLink>
+        </div>
+        {/* Routes, Route 練習區 */}
+        {/* 練習區 */}
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
